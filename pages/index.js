1c64548a5687ec8d851aa0e305480aa5c40a7831
Juan Manuel <jmanuelgrehuello@yahoo.com>
2022-07-04 15:01:46 -0300

    comienzo de clonado tw

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Home () {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Twitter</title>
+        <link rel="icon" href="/twitter.ico" />
+      </Head>
+      
+      <main>
+        {/* Side Bar */}
+        
+        {/* Feed */}
+        {/* Widgets */}
+      </main>
+
+    </div>
+  );
+};
